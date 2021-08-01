4cf66b53a621a9c7256b24e5d82398437f50ade8
Simone <marzolla.istruzione@gmail.com>
2021-08-01 16:32:12 +0200

    Swi Load

---
+++ __resource.lua
@@ -0,0 +1,10 @@
+files {
+    'style.css',
+    'nrloading.mp4',
+    'multimedia/nrload.mp3',
+    'load.html',
+}
+
+loadscreen 'load.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
